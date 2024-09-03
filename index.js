f9c713f11cbf1598cc21553f033dd90ad73e176e
Divyanshu Pal <86774540+divyanshu-pal@users.noreply.github.com>
2024-09-03 17:41:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 import express from 'express';
-
-
 import connectDB from './config/connectDB.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
 /**[ Middlewares ]**/
 import errorHandler from './middleware/errorHandler.js';
 import appMiddleware from './middleware/app.middleware.js';
-
 import router from './routes/order.routes.js';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 dotenv.config();
+app.use(express.urlencoded({extended:true}));
 // const main = async () => {
   const port = process.env.PORT||4000;
 
   // try {
-    const app = express();
-    appMiddleware(app);
+    
+    // appMiddleware(app);
     /**[ Register Route ]**/
     app.use('/api',router)
 
-    connectDB()
+    await connectDB()
     app.use(errorHandler)   
     app.listen(port, () => {
       console.log(`listening:*${port}`);
