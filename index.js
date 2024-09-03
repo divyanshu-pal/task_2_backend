0573f29e2d38bb3a3bf86f31388edad6844d3ea2
Divyanshu Pal <86774540+divyanshu-pal@users.noreply.github.com>
2024-09-03 19:49:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 import express from 'express';
 import connectDB from './config/connectDB.js';
 import dotenv from 'dotenv'
-import cors from 'cors'
 /**[ Middlewares ]**/
 import errorHandler from './middleware/errorHandler.js';
 import appMiddleware from './middleware/app.middleware.js';
 import router from './routes/order.routes.js';
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3001', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // Allow cookies to be sent with requests (optional)
-}));
-app.use(express.json());
-dotenv.config();
-app.use(express.urlencoded({extended:true}));
-// const main = async () => {
+
+
+
+const main = async () => {
+  dotenv.config();
   const port = process.env.PORT||4000;
 
-  // try {
+  try {
     
-    // appMiddleware(app);
-    /**[ Register Route ]**/
+     appMiddleware(app);
+   
     app.use('/api',router)
 
-      connectDB();
-    // app.use(errorHandler)   
+    await connectDB()
+    app.use(errorHandler);   
     app.listen(port, () => {
       console.log(`listening:*${port}`);
     });
 
-  // } catch (e) {}
-// };
+  } catch (e) {}
+};
 
-// main().catch((e) => console.error(e));
+main().catch((e) => console.error(e));
